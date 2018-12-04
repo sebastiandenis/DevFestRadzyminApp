41c9dc384608e6c02ec640053bbd405fb3d02091
Sebastian Denis <sebastian.denis@outlook.com>
2018-12-04 01:08:58 +0100

    initial

---
+++ src/app/speakers/speakers.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "Speakers",
+    moduleId: module.id,
+    templateUrl: "./speakers.component.html"
+})
+export class SpeakersComponent implements OnInit {
+    constructor() {
+        // Use the component constructor to inject providers.
+    }
+
+    ngOnInit(): void {
+        // Use the "ngOnInit" handler to initialize data for the view.
+    }
+}
